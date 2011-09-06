9ef2f390934e9b8fdcc39a805d01851050a98a62
Scott Chamberlain <myrmecocystus@gmail.com>
2011-09-06 09:07:22 -0500

    edited fxn getsampleids

--- R/getsampleids.R
+++ R/getsampleids.R
@@ -7,17 +7,18 @@ getsampleids <-
 #   geoinc: geographic location to include (character)
 #   geoexc: geographic location to exclude (character)
 # Examples:
-#   getsampleids('Formicidae')
-#   getsampleids('Andrenidae')
+#   getsampleids(taxoninc = 'Andrena')
+#   getsampleids(taxoninc = 'Andrenidae')
 
-function(taxoninc, taxonexc = NA, geoinc = NA, geoexc = NA, 
-    url = "http://services.boldsystems.org/eSearch.php?") {
+function(taxoninc = NA, taxonexc = NA, geoinc = NA, geoexc = NA, 
+    url = "http://services.boldsystems.org/eSearch.php?",
+    ...) {
   
   taxoninc_ <- paste("taxon_inc=(", taxoninc, ")", sep='')
   if (!is.na(taxonexc)) {taxonexc_ <- paste('&taxon_exc=(', taxonexc, ")", sep='')} else {taxonexc_ <- NULL}
   if (!is.na(geoinc)) {geoinc_ <- paste('&geo_inc=(', geoinc, ")", sep='')} else {geoinc_ <- NULL}
   if (!is.na(geoexc)) {geoexc_ <- paste('&geo_exc=(', geoexc, ")", sep='')} else {geoexc_ <- NULL}
-  taxurl <- paste(url, taxoninc_, taxonexc_, geoinc_, geoexc_, "&return_type=xml", sep='')
+  taxurl <- paste(url, taxoninc_, taxonexc_, geoinc_, geoexc_, "&return_type=xml", sep='')  
   out <- getURL(taxurl)
   reclength <- length(xmlTreeParse(out)[[1]][[1]])
   outlist <- xmlToList(out)
@@ -25,3 +26,5 @@ function(taxoninc, taxonexc = NA, geoinc = NA, geoexc = NA,
   
   return(sampidlist)
 }
+
+
--- R/rbold_search.R
+++ R/rbold_search.R
@@ -1,24 +1,47 @@
-url <- "http://services.boldsystems.org/eSearch.php?taxon_inc=(Cottus)&geo_inc=(Canada)&geo_exc=(Quebec,Ontario,New Brunswick)&return_type=json"
-getURL(url)
+url <- "http://services.boldsystems.org/eSearch.php?taxon_inc=(Delphinus)&return_type=json"
+fromJSON(url)
 
 #
 url <- "http://services.boldsystems.org/eFetch.php?record_type=full&id_type=sampleid&ids=(08-SRNP-102876)&return_type=json"
 fromJSON(url)
 
-# Function to get sequences for a taxonomic group
-url <- "http://services.boldsystems.org/eSearch.php?taxon_inc=(Formicidae)&geo_exc=(Canada)&return_type=xml"
-out <- getURL(url)
-reclength <- length(xmlTreeParse(out)[[1]][[1]])
-outlist <- xmlToList(out)
-laply(outlist[1:reclength], function(x) x$specimen_identifiers$sampleid, .progress="text")
-laply(outlist[1:10], getseqs, .progress="text")
-getseqs <- function(x) {
-  sampleid_ <- x$specimen_identifiers$sampleid
-  url <- paste("http://services.boldsystems.org/eFetch.php?record_type=sequence&id_type=sampleid&ids=(", sampleid_, ")&return_type=json", sep='')
-  seq_ <- fromJSON(url)$record$nucleotides
-  return(seq_)
-}
 
+# rbold_search.R
+
+rbold_search <- 
+# Args:
+#   sciname: scientitic name of taxon, as e.g., "Cottus" (character)
+#   geoexc: geographic area to exclude (character)
+#   returntype: json or xml (character)
+# Examples:
+#   rbold_search(sciname = "Formicidae")
+#   rbold_search("Formicidae", "Canada")
+
+function(sciname = NA, geoexc = NA, returntype = NA,
+  url = 'http://services.boldsystems.org/eSearch.php?',
+  ..., 
+  curl = getCurlHandle() ) 
+{
+#   url <- "http://services.boldsystems.org/eSearch.php?taxon_inc=(Formicidae)&geo_exc=(Canada)&return_type=xml"
+  
+  url2 <- paste(url, "?", sep = "")
+  taxon_inc = paste("(", sciname, ")", sep = ""))
+  args <- list(taxon_inc = sciname)
+
+  out <- getURL(url)
+  reclength <- length(xmlTreeParse(out)[[1]][[1]])
+  outlist <- xmlToList(out)
+  laply(outlist[1:reclength], function(x) x$specimen_identifiers$sampleid, .progress="text")
+  
+  getseqs <- function(x) {
+    sampleid_ <- x$specimen_identifiers$sampleid
+    url <- paste("http://services.boldsystems.org/eFetch.php?record_type=sequence&id_type=sampleid&ids=(", sampleid_, ")&return_type=json", sep='')
+    seq_ <- fromJSON(url)$record$nucleotides
+    return(seq_)
+  }
+  
+  laply(outlist[1:10], getseqs, .progress="text")
+}
 
   
   
@@ -27,8 +50,7 @@ getseqs <- function(x) {
 
 
 url <- "http://services.boldsystems.org/eSearch.php?taxon_inc=(Saturniidae)&geo_exc=(Canada)&return_type=text"
-out <- 
-  read.table(url, header = T, sep = "\t", nrow=100)
+out <- read.table(url, header = T, sep = "\t", nrow=100)
 str(out)
 
 
