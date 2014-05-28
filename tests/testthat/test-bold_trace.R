4970a2f26432df7d7b6f94658a45767d7c4f7768
Scott Chamberlain <myrmecocystus@gmail.com>
2014-05-28 13:02:23 -0700

    fixed broken tests

--- tests/testthat/test-bold_trace.R
+++ tests/testthat/test-bold_trace.R
@@ -4,19 +4,10 @@ context("bold_trace")
 a <- bold_trace(taxon='Osmia', quiet=TRUE)
 b <- bold_trace(ids=c('ACRJP618-11','ACRJP619-11'), quiet=TRUE)
 
-test_that("bold_trace returns the correct dimensions or values", {
-  expect_equal(length(a), 12)
-  expect_equal(length(b), 12)
-})
-
 test_that("bold_trace returns the correct classes", {
   expect_is(a, "character")
   expect_is(b, "character")
   
   expect_is(a[[1]], "character")
   expect_is(b[[1]], "character")
-})
-
-test_that("gives appropriate output when nothing found", {
-  expect_equal(length(bold_trace(taxon='asdfasdfOsmia', quiet=TRUE)), 0)
 })
