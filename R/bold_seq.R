64e8f1a6bdf957ecaf6ed99df71cf7781e30d353
Scott Chamberlain <myrmecocystus@gmail.com>
2016-12-06 10:03:35 -0800

    tidying bold_seq man file

--- R/bold_seq.R
+++ R/bold_seq.R
@@ -1,19 +1,23 @@
 #' Search BOLD for sequences.
 #'
-#' Get sequences for a taxonomic name, id, bin, container, institution, researcher, geographic
-#' place, or gene.
+#' Get sequences for a taxonomic name, id, bin, container, institution, 
+#' researcher, geographic, place, or gene.
 #'
 #' @importFrom stringr str_replace_all str_replace str_split
 #' @export
 #' @template args
 #' @template otherargs
-#' @references \url{http://www.boldsystems.org/index.php/resources/api#sequenceParameters}
+#' @references 
+#' \url{http://www.boldsystems.org/index.php/resources/api#sequenceParameters}
 #'
-#' @param marker (character) Returns all records containing matching marker codes.
+#' @param marker (character) Returns all records containing matching 
+#' marker codes.
 #'
-#' @return A list with each element of length 4 with slots for id, name, gene, and sequence.
+#' @return A list with each element of length 4 with slots for id, name, 
+#' gene, and sequence.
+#' 
 #' @examples \dontrun{
-#' bold_seq(taxon='Coelioxys')
+#' res <- bold_seq(taxon='Coelioxys')
 #' bold_seq(taxon='Aglae')
 #' bold_seq(taxon=c('Coelioxys','Osmia'))
 #' bold_seq(ids='ACRJP618-11')
@@ -31,19 +35,31 @@
 #' res$headers
 #'
 #' ## curl debugging
-#' ### You can do many things, including get verbose output on the curl call, and set a timeout
+#' ### You can do many things, including get verbose output on the curl 
+#' ### call, and set a timeout
 #' library("httr")
 #' bold_seq(taxon='Coelioxys', config=verbose())[1:2]
 #' # bold_seqspec(taxon='Coelioxys', config=timeout(0.1))
 #' }
 
-bold_seq <- function(taxon = NULL, ids = NULL, bin = NULL, container = NULL, institutions = NULL,
-  researchers = NULL, geo = NULL, marker = NULL, response=FALSE, ...) {
-  args <- bc(list(taxon=pipeornull(taxon), geo=pipeornull(geo), ids=pipeornull(ids),
-      bin=pipeornull(bin), container=pipeornull(container), institutions=pipeornull(institutions),
-      researchers=pipeornull(researchers), marker=pipeornull(marker)))
-  check_args_given_nonempty(args, c('taxon','ids','bin','container','institutions','researchers',
-                                    'geo','marker'))
+bold_seq <- function(taxon = NULL, ids = NULL, bin = NULL, container = NULL, 
+  institutions = NULL, researchers = NULL, geo = NULL, marker = NULL, 
+  response=FALSE, ...) {
+  
+  args <- bc(
+    list(
+      taxon = pipeornull(taxon), geo = pipeornull(geo), 
+      ids = pipeornull(ids), bin = pipeornull(bin), 
+      container = pipeornull(container), 
+      institutions = pipeornull(institutions),
+      researchers = pipeornull(researchers), marker = pipeornull(marker)
+    )
+  )
+  check_args_given_nonempty(
+    args, 
+    c('taxon','ids','bin','container','institutions','researchers',
+      'geo','marker')
+  )
   out <- b_GET(paste0(bbase(), 'API_Public/sequence'), args, ...)
   if (response) { 
     out 
