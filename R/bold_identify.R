6d6d46c6b877956bdc251fb6133e28960dfb10e7
Scott Chamberlain <myrmecocystus@gmail.com>
2016-12-06 10:13:08 -0800

    fix #37 - fixed wrong documentation for bold_identify

--- R/bold_identify.R
+++ R/bold_identify.R
@@ -4,24 +4,28 @@
 #'
 #' @param sequences (character) Returns all records containing matching marker 
 #' codes. Required.
-#' @param db (character) The database to match against, one of COX1, COX1_SPECIES,
-#' COX1_SPECIES_PUBLIC, OR COX1_L604bp. See Details for more information.
-#' @param response (logical) Note that response is the object that returns from the
-#' Curl call, useful for debugging, and getting detailed info on the API call.
-#' @param ... Further args passed on to httr::GET, main purpose being curl debugging
+#' @param db (character) The database to match against, one of COX1, 
+#' COX1_SPECIES, COX1_SPECIES_PUBLIC, OR COX1_L604bp. See Details for 
+#' more information.
+#' @param response (logical) Note that response is the object that returns 
+#' from the Curl call, useful for debugging, and getting detailed info on 
+#' the API call.
+#' @param ... Further args passed on to httr::GET, main purpose being 
+#' curl debugging
 #'
 #' @details Detailed description of options for the db parmeter:
 #'
 #' \itemize{
-#'  \item COX1 Every COI barcode record with a species level identification and
-#'  a minimum sequence length of 500bp. This includes many species represented by
-#'  only one or two specimens as well as  all species with interim taxonomy.
-#'  \item COX1_SPECIES Every COI barcode record on BOLD with a minimum sequence
+#'  \item COX1 Every COI barcode record on BOLD with a minimum sequence
 #'  length of 500bp (warning: unvalidated library and includes records without
 #'  species level identification). This includes many species represented by
 #'  only one or two specimens as well as all species with interim taxonomy. This
 #'  search only returns a list of the nearest matches and does not provide a
 #'  probability of placement to a taxon.
+#'  \item COX1_SPECIES Every COI barcode record with a species level 
+#'  identification and a minimum sequence length of 500bp. This includes 
+#'  many species represented by only one or two specimens as well as  all 
+#'  species with interim taxonomy.
 #'  \item COX1_SPECIES_PUBLIC All published COI records from BOLD and GenBank
 #'  with a minimum sequence length of 500bp. This library is a collection of
 #'  records from the published projects section of BOLD.
@@ -31,7 +35,8 @@
 #'  with short reads from the barcode region of COI.
 #' }
 #' @return A data.frame with details for each specimen matched.
-#' @references \url{http://www.boldsystems.org/index.php/resources/api?type=idengine}
+#' @references 
+#' \url{http://www.boldsystems.org/index.php/resources/api?type=idengine}
 #' @examples \dontrun{
 #' seq <- sequences$seq1
 #' head(bold_identify(sequences=seq)[[1]])
@@ -61,7 +66,8 @@ bold_identify <- function(sequences, db = 'COX1', response=FALSE, ...) {
       tt <- content(out, "text", encoding = "UTF-8")
       xml <- xml2::read_xml(tt)
       nodes <- xml2::xml_find_all(xml, "//match")
-      toget <- c("ID","sequencedescription","database","citation","taxonomicidentification","similarity")
+      toget <- c("ID","sequencedescription","database",
+                 "citation","taxonomicidentification","similarity")
       outlist <- lapply(nodes, function(x){
         tmp2 <- vapply(toget, function(y) {
           tmp <- xml2::xml_find_first(x, y)
@@ -69,7 +75,8 @@ bold_identify <- function(sequences, db = 'COX1', response=FALSE, ...) {
         }, "")
         spectmp <- xml2::as_list(xml2::xml_find_first(x, "specimen"))
         spectmp <- unnest(spectmp)
-        names(spectmp) <- c('specimen_url','specimen_country','specimen_lat','specimen_lon')
+        names(spectmp) <- c('specimen_url','specimen_country',
+                            'specimen_lat','specimen_lon')
         spectmp[sapply(spectmp, is.null)] <- NA
         data.frame(c(tmp2, spectmp), stringsAsFactors = FALSE)
       })
