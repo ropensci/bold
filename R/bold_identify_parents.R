667d0f8099844726e00bd449c39ad2f922bbcac7
Scott Chamberlain <myrmecocystus@gmail.com>
2018-10-25 12:01:31 -0700

    #50 more work on filtering for bold id parents

--- R/bold_identify_parents.R
+++ R/bold_identify_parents.R
@@ -6,7 +6,21 @@
 #' same. required.
 #' @param wide (logical) output in long or wide format. See Details.
 #' Default: \code{FALSE}
-#'
+#' @param taxid  (character) A taxid name. Optional. See `Filtering` below.
+#' @param taxon (character) A taxon name. Optional. See `Filtering` below.
+#' @param tax_rank (character) A tax_rank name. Optional. See `Filtering` 
+#' below.
+#' @param tax_division (character) A tax_division name. Optional. See 
+#' `Filtering` below.
+#' @param parentid (character) A parentid name. Optional. See `Filtering` 
+#' below.
+#' @param parentname (character) A parentname name. Optional. See `Filtering` 
+#' below.
+#' @param taxonrep (character) A taxonrep name. Optional. See `Filtering` 
+#' below.
+#' @param specimenrecords (character) A specimenrecords name. Optional. 
+#' See `Filtering` below.
+#' 
 #' @details This function gets unique set of taxonomic names from the input
 #' data.frame, then queries \code{\link{bold_tax_name}} to get the
 #' taxonomic ID, passing it to \code{\link{bold_tax_id}} to get the parent
@@ -15,6 +29,17 @@
 #' Records in the input data that do not have matches for parent names
 #' simply get NA values in the added columns.
 #'
+#' @section Filtering:
+#' The parameters `taxid`, `taxon`, `tax_rank`, `tax_division`, 
+#' `parentid`, `parentname`,`taxonrep`, and `specimenrecords` are not used 
+#' in the search sent to BOLD, but are used in filtering the data down 
+#' to a subset that is closer to the target you want. For all these 
+#' parameters, you can use regex strings since we use [grep()] internally 
+#' to match. Filtering narrows down to the set that matches your query, 
+#' and removes the rest. The data.frame that we filter on with these 
+#' parameters internally is the result of a call to the [bold_tax_name()]
+#' function. 
+#' 
 #' @section wide vs long format:
 #' When \code{wide = FALSE} you get many rows for each record. Essentially,
 #' we \code{cbind} the taxonomic classification onto the one row from the
@@ -79,17 +104,14 @@ bold_identify_parents.list <- function(x, wide = FALSE, taxid = NULL,
     tmp <- bold_tax_name(w)
     # if length(tmp) > 1, user decides which one
     if (NROW(tmp) > 1) {
-      # if (is.null(pick_parent)) {
-      #   message("more than 1 parent, but pick_parent is NULL, skipping")
-      # } else {
-        # pick_parent = list(tax_division = "Animals")
-        # pick_parent = 'tax_division == "Animals"'
-        # choose <- list(tmp = pick_parent)
-        # tmp[parse(text = paste0("tmp$", pick_parent)), ]
-        # subset(tmp, parse(text = pick_parent))
-        tmp <- filt(tmp, "taxid", taxid)
-        tmp <- filt(tmp, "tax_division", tax_division)
-      # }
+      tmp <- filt(tmp, "taxid", taxid)
+      tmp <- filt(tmp, "taxon", taxon)
+      tmp <- filt(tmp, "tax_rank", tax_rank)
+      tmp <- filt(tmp, "tax_division", tax_division)
+      tmp <- filt(tmp, "parentid", parentid)
+      tmp <- filt(tmp, "parentname", parentname)
+      tmp <- filt(tmp, "taxonrep", taxonrep)
+      tmp <- filt(tmp, "specimenrecords", specimenrecords)
     }
     if (!is.null(tmp$taxid)) {
       tmp2 <- bold_tax_id(tmp$taxid, includeTree = TRUE)
