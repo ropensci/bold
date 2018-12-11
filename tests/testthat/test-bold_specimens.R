e0b6c9d23f6babe3aeb494c2839300b77be4e63b
Scott Chamberlain <myrmecocystus@gmail.com>
2018-12-10 19:09:27 -0800

    remove bold_specimens fixture, causing problem on R devel on linux

--- tests/testthat/test-bold_specimens.R
+++ tests/testthat/test-bold_specimens.R
@@ -1,28 +1,9 @@
 # tests for bold_specimens fxn in bold
 context("bold_specimens")
 
-test_that("bold_specimens returns the correct dimensions or values", {
-  skip_on_cran()
-
-  vcr::use_cassette("bold_specimens", {
-    a <- bold_specimens(taxon='Osmia')
-    expect_is(a, "data.frame")
-    expect_is(a$recordID, "integer")
-    expect_is(a$processid, "character")
-  }, preserve_exact_body_bytes = TRUE)
-
-  vcr::use_cassette("bold_specimens_response", {
-    b <- bold_specimens(taxon='Osmia', format='xml', response=TRUE)
-    expect_equal(b$status_code, 200)
-    expect_equal(b$response_headers$`content-type`, "application/x-download")
-    expect_is(b, "HttpResponse")
-    expect_is(b$response_headers, "list")
-  }, preserve_exact_body_bytes = FALSE)
-})
-
 test_that("Throws warning on call that takes forever including timeout in callopts", {
   skip_on_cran()
-
+  
   expect_error(bold_specimens(geo='Costa Rica', timeout_ms = 2), "Timeout was reached")
 })
 
@@ -31,4 +12,24 @@ test_that("bold_seq returns correct thing when parameters empty or not given", {
 
   expect_error(bold_specimens(taxon=''), "must provide a non-empty value")
   expect_error(bold_specimens(), "must provide a non-empty value")
+})
+
+test_that("bold_specimens returns the correct dimensions or values", {
+  skip_on_cran()
+
+  # FIXME: causing vcr problem on R devel (3.6 or 3.7)
+  # vcr::use_cassette("bold_specimens", {
+  #   a <- bold_specimens(taxon='Osmia')
+  #   expect_is(a, "data.frame")
+  #   expect_is(a$recordID, "integer")
+  #   expect_is(a$processid, "character")
+  # }, preserve_exact_body_bytes = TRUE)
+
+  vcr::use_cassette("bold_specimens_response", {
+    b <- bold_specimens(taxon='Osmia', format='xml', response=TRUE)
+    expect_equal(b$status_code, 200)
+    expect_equal(b$response_headers$`content-type`, "application/x-download")
+    expect_is(b, "HttpResponse")
+    expect_is(b$response_headers, "list")
+  }, preserve_exact_body_bytes = FALSE)
 })
