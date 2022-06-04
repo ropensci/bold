f3b7cd919c8e98a6e118d9366db2a072776a2272
Salix <31168746+salix-d@users.noreply.github.com>
2022-06-04 18:01:14 -0400

    added arguments checks, added filters

--- R/bold_tax_name.R
+++ R/bold_tax_name.R
@@ -4,12 +4,12 @@
 #' @param name (character) One or more scientific names. required.
 #' @param fuzzy (logical) Whether to use fuzzy search or not (default: `FALSE`)
 #' @template otherargs
-#' @references 
+#' @references
 #' http://v4.boldsystems.org/index.php/resources/api?type=taxonomy
-#' @details The `dataTypes` parameter is not supported in this function. 
-#' If you want to use that parameter, get an ID from this function and pass 
+#' @details The `dataTypes` parameter is not supported in this function.
+#' If you want to use that parameter, get an ID from this function and pass
 #' it into `bold_tax_id`, and then use the `dataTypes` parameter.
-#' @seealso [bold_tax_id()]
+#' @seealso \code{\link[bold]{bold_tax_id()}}
 #' @examples \dontrun{
 #' bold_tax_name(name='Diplura')
 #' bold_tax_name(name='Osmia')
@@ -31,29 +31,52 @@
 #' bold_tax_name(name='Diplura', verbose = TRUE)
 #' }
 
-bold_tax_name <- function(name, fuzzy = FALSE, response = FALSE, ...) {
+bold_tax_name <- function(name, fuzzy = FALSE, response = FALSE,
+                          tax_division = NULL, tax_rank = NULL, ...) {
+  assert(name, "character")
+  if(!missing(fuzzy)) assert(fuzzy, "logical")
+  if(length(tax_division)){
+    assert(tax_division, "character")
+    if(!all(tax_division %in% c("Animalia", "Protista", "Fungi", "Plantae")))
+      stop("'tax_division' must be one or more of ",
+           c("Animalia", "Protista", "Fungi", "Plantae"))
+  }
+  if(length(tax_rank)){
+    assert(tax_rank, "character")
+    tax_rank <- tolower(tax_rank)
+    if(!tax_rank %in% names(all_ranks))
+      stop("Invalid tax_rank name.")
+  }
 
-  tmp <- lapply(name, function(x)
-    get_response(bc(list(taxName = x, fuzzy = if (fuzzy) 'true' else NULL)),
-                 url = paste0(bbase(), "API_Tax/TaxonSearch"), ...)
+  res <- lapply(`names<-`(name, name), function(x)
+    get_response(args = c(taxName = x, fuzzy = fuzzy),
+                 url =b_url("API_Tax/TaxonSearch"), ...)
   )
   if (response) {
-    tmp 
+    res
   } else {
-    (vvv <- data.table::setDF(data.table::rbindlist(
-      Map(process_tax_name, tmp, name), 
-      use.names = TRUE, fill = TRUE)
-    ))
+    out <- setrbind(lapply(res, process_tax_name,
+                           tax_division = tax_division,
+                           tax_rank = tax_rank),
+                    idcol = "input")
+    w <- vapply(res, `[[`, "", "warning")
+    attr(out, "errors") <- bc(w[nzchar(w)])
+    out
   }
 }
 
-process_tax_name <- function(x, y) {
-  tt <- rawToChar(x$content)
-  out <- if (x$status_code > 202) "stop" else 
-    jsonlite::fromJSON(tt, flatten = TRUE)
-  if (length(out) == 0 || identical(out[[1]], list()) || all(out == "stop")) {
-    data.frame(input = y, stringsAsFactors = FALSE)
+process_tax_name <- function(x, tax_division, tax_rank) {
+  if(!nzchar(x$warning)){
+    out <- jsonlite::fromJSON(x$response$parse("UTF-8"), flatten = TRUE)
+    if (length(out) && length(out$top_matched_names)){
+      out <- data.frame(out$top_matched_names, stringsAsFactors = FALSE)
+      if(length(tax_division) && length(out$tax_division)) out <- out[out$tax_division %in% tax_division,]
+      if(length(tax_rank) && length(out$tax_rank)) out <- out[out$tax_rank %in% tax_rank,]
+    } else {
+      out <- data.frame(taxid = NA)
+    }
   } else {
-    data.frame(out$top_matched_names, input = y, stringsAsFactors = FALSE)
+    out <- data.frame(taxid = NA)
   }
+  out
 }
