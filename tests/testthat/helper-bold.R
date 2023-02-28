afabe541c3188ebcc1a7aa86c0ef47b24a3afc81
Salix <31168746+salix-d@users.noreply.github.com>
2023-02-28 13:20:26 -0500

    fixing test (2nd attempt)

--- tests/testthat/helper-bold.R
+++ tests/testthat/helper-bold.R
@@ -1,3 +1,3 @@
 # set up vcr
 library("vcr")
-invisible(vcr::vcr_configure(dir = normalizePath("../fixtures"), write_disk_path = normalizePath("bold_trace_files")))
+invisible(vcr::vcr_configure(dir = normalizePath("../fixtures"), write_disk_path = normalizePath("../bold_trace_files")))
