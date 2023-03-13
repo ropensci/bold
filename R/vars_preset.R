c8f47dde174f5f3046edb36b5e4cc89cdbe02531
Salix <31168746+salix-d@users.noreply.github.com>
2023-03-12 20:09:17 -0400

    new export

---
+++ R/vars_preset.R
@@ -0,0 +1,25 @@
+b_dataTypes <- c(
+  "basic",
+  "stats",
+  "geo",
+  "images",
+  "sequencinglabs",
+  "depository",
+  "thirdparty",
+  "all"
+)
+b_tax_division <- c("Animalia", "Protista", "Fungi", "Plantae")
+b_tax_rank <-
+  c(
+    "kingdom",
+    "phylum",
+    "class",
+    "order",
+    "family",
+    "subfamily",
+    "tribe",
+    "genus",
+    "species",
+    "subspecies"
+  )
+b_db <- c('COX1', 'COX1_SPECIES', 'COX1_SPECIES_PUBLIC', 'COX1_L640bp')
